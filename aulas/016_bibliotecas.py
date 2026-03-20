43aba82a038ee2d5559ca21188054cec93b68446
Caio Riciati <esdrascaio5313@gmail.com>
2026-03-20 00:26:44 -0300

    analisando dataset

--- aulas/016_bibliotecas.py
+++ aulas/016_bibliotecas.py
@@ -1,8 +1,9 @@
 import pandas as pd
-from ydata_profiling import ProfileReport
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
 
-basec= pd.read_csv("C:\Users\esdra\OneDrive\Desktop\Aulas-machine-learning-em-python\aulas\credit_data.csv")
+basec = pd.read_csv(r"C:\Users\esdra\OneDrive\Desktop\Aulas-machine-learning-em-python\aulas\credit_data.csv")
+print(basec.head())
+print(basec.describe())

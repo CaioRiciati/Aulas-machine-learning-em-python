7fd201a5e4de40563075ca9ad526a01049bdc170
Caio Riciati <esdrascaio5313@gmail.com>
2026-04-11 23:52:26 -0300

    Census Database

---
+++ aulas/022_baseDadosCenso.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import seaborn as sb
+import matplotlib.pyplot as plt
+import plotly.express as px
+import numpy as np
+
+census = pd.read_csv(r"C:\Users\esdra\OneDrive\Desktop\Aulas-machine-learning-em-python\Bases de dados\census.csv")
+
+print(census)

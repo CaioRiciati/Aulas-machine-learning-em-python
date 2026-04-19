e19a8086e12ebc0e6f86c752a3cd111f73c02fc0
Caio Riciati <esdrascaio5313@gmail.com>
2026-04-19 20:32:47 -0300

    mais tipos de vizualização de dados

--- aulas/022_baseDadosCenso.py
+++ aulas/022_baseDadosCenso.py
@@ -6,4 +6,18 @@ import numpy as np
 
 census = pd.read_csv(r"C:\Users\esdra\OneDrive\Desktop\Aulas-machine-learning-em-python\Bases de dados\census.csv")
 
-print(census)
+c2 =census.isnull().sum()
+
+print(np.unique(census['income'], return_counts=True))
+sb.countplot( x=census['income'])
+
+#plt.hist(census['age'])
+plt.hist(census['education-num'])
+
+grafico2 = px.treemap(census, path=['workclass'])
+grafico3 = px.treemap(census, path=['workclass', 'age'])
+grafico = px.treemap(census, path=['occupation','income','relationship'])
+
+
+graf = px.parallel_categories(census, dimensions=['workclass', 'age', 'income'])
+graf.show()

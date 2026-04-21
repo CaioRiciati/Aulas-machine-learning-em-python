6fe56454cdb7f4d3b8829c178758f8732a62f96f
Caio Riciati <esdrascaio5313@gmail.com>
2026-04-20 23:04:18 -0300

    OneHotEncoder e escalonamento dos dados

--- aulas/025_labelEncoder.py
+++ aulas/025_labelEncoder.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
 
 census = pd.read_csv(r"Bases de dados/census.csv")
 xCensus = census.iloc[:, 0:14].to_numpy()
@@ -29,4 +32,24 @@ xCensus[:,9] =sex.fit_transform(xCensus[:,9])
 xCensus[:,13] = nativeCountry.fit_transform(xCensus[:,13])
 
 
+
+
+onehotencoder = ColumnTransformer(transformers=[('OneHot', OneHotEncoder(), [1,3,5,6,7,8,9,13])] , remainder ='passthrough')
+
+
+xCensus = onehotencoder.fit_transform(xCensus).toarray()
+
+
+#fazendo o escalonamento dos dados
+scalerCensus = StandardScaler()
+xCensus = scalerCensus.fit_transform(xCensus)
+
+
+
 print(xCensus[0])
+
+
+
+
+#p contar qnts atributos categoricos tem cada coluna(aki no caso é na colina workclass)
+len(np.unique(census['workclass']))

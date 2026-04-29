e158299a714d01c1a97874eafd9cb9cee2c9abf9
Caio Riciati <esdrascaio5313@gmail.com>
2026-04-29 18:51:22 -0300

    separando a bases de  dados

---
+++ aulas/029_baseDeTreinamento.py
@@ -0,0 +1,33 @@
+from sklearn.model_selection import train_test_split
+import plotly.express as px
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import seaborn as sb
+
+table =  pd.read_csv(r"C:\Users\caio.eariciati\Desktop\Aulas-machine-learning-em-python-main\aulas\credit_data.csv")
+
+
+
+#acho a media e substituo os negativos pela media
+media = table.loc[table['age']> 0, 'age'].mean()
+table.loc[table['age']<0, 'age'] = media
+
+
+#preenncho os vazios pela media
+table['age'] = table['age'].fillna(table['age'].mean())
+
+
+
+
+
+
+
+
+#separando previsores 
+xcredit = table.iloc[:,1:4].to_numpy()
+ycredit = table.iloc[:,4].to_numpy()
+
+xCreditTrei, xCreditTest, yCreditTrei, yCreditTest = train_test_split(xcredit, ycredit , test_size = 0.25, random_state = 0)
+
+print(xCreditTest.shape)
